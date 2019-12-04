28996dc4f9560a2a583d1e998fc6c9e235c8d0f5
edrahim108 <a15s108@outlook.com>
2019-12-04 08:08:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var vid = document.getElementById("gossVideo");
+var vid1 = document.getElementsByTagName('VIDEO')[1];
+var vid2 = document.getElementsByTagName('VIDEO')[2];
+
+function playVid() {
+    vid.play();
+}function playVid1() {
+    vid1.play();
+}function playVid2() {
+    vid2.play();
+}
+
+function pauseVid() {
+    vid.pause();
+}
+function pauseVid1() {
+    vid1.pause();
+}
+function pauseVid2() {
+    vid2.pause();
+}
